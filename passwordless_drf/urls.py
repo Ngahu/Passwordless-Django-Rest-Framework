8befe86e381dc567a4c76075314d0e8fe5e45da8
joel Ngahu <ngahu@joels-MacBook-Pro.local>
2019-05-14 15:59:53 +0300

    add the config to render the static files .

--- passwordless_drf/urls.py
+++ passwordless_drf/urls.py
@@ -18,9 +18,19 @@ from django.contrib import admin
 from rest_framework.authtoken import views
 
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^accounts/', include('accounts.urls',namespace='accounts_auth')),
 ]
+
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
