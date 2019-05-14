248f1d50ab8b04badd3912d7af69beae0b04f67e
joel Ngahu <ngahu@joels-MacBook-Pro.local>
2019-05-14 15:53:12 +0300

    Add of the allowed host.

--- passwordless_drf/settings.py
+++ passwordless_drf/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['passwordlessdrf.pythonanywhere.com']
 
 
 AUTH_USER_MODEL = 'accounts.User'
